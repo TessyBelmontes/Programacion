3bc13069034450740e3e452c4b015e8252acd2ee
TessyBelmontes <tealbel@icloud.com>
2017-02-02 18:11:25 -0600

    Practica2

---
+++ Practicas/Variables.java
@@ -0,0 +1,27 @@
+
+
+
+public class Variables{
+
+static String var6;
+
+public static void main(String[]args){
+byte var1=127;
+short var2=32767;
+int var3=2147483647;
+long var4=9223372036854775807L;
+char var5='2';
+String var7="Hola java";
+
+System.out.println("Variables");
+
+
+System.out.println("byte var:"+var1);
+System.out.println("short var:"+var2);
+System.out.println("int var:"+var3);
+System.out.println("long var:"+var4);
+System.out.println("char var:"+var5);
+System.out.println("String var:"+var6);
+System.out.println("String var:"+var7);
+}
+}
---
+++ Practicas/Variables2.java
@@ -0,0 +1,27 @@
+public class Variables2{
+
+static byte var1;
+static short var2;
+static int var3;
+static long var4;
+static double var5;
+static char var6;
+static boolean var7;
+
+static String var8;
+
+public static void main(String[]args){
+System.out.println("Variables");
+
+System.out.println("byte var:"+var1);
+System.out.println("short var:"+var2);
+System.out.println("int var:"+var3);
+System.out.println("long var:"+var4);
+System.out.println("double var:"+var5);
+System.out.println("char var:"+var6);
+System.out.println("boolean var:"+var7);
+System.out.println("string var:"+var8);
+
+}
+
+}
