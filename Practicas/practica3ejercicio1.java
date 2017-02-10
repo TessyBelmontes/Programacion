2b8a2b0fe4d5a141b2caa59f5bd9280b1812c8af
TessyBelmontes <tealbel@icloud.com>
2017-02-09 18:19:28 -0600

    Practica3-1

--- Practicas/practica3ejercicio1.java
+++ Practicas/practica3ejercicio1.java
@@ -2,24 +2,23 @@ public class practica3ejercicio1{
 
 public static void main(String[]args){
 int result = 0;
-int a = 3;
 
-result = result + 3;
+result+=3;
 System.out.println("result is now "+result);
 
-result = result - 1;  
+result-=1;  
 System.out.println("result is now "+result);
 
-result = result*2;
+result*=2;
 System.out.println("result is now "+result);
 
-result = result/2;
+result/=2;
 System.out.println("result is now "+result);
 
-result = result*5;
+result*=5;
 System.out.println("result is now "+result);
 
-result = (result/2)-2;
+result-=7;
 System.out.println("result is now "+result);
 
  }
