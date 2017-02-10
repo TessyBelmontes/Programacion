739ec1e58a2584056bb0c7b2a23e4ee074d6be43
TessyBelmontes <tealbel@icloud.com>
2017-02-10 09:12:02 -0600

    Add files via upload

---
+++ Practicas/practica3ejercicio3.java
@@ -0,0 +1,37 @@
+public class practica3ejercicio3{
+
+public static void main(String[]args){
+//int month=1;
+int month2=2;
+//int month3=3;
+//int month4=4;
+//int month5=5;
+//int month6=6;
+//int month7=7;
+//int month8=8;
+//int month9=9;
+//int month10=10;
+//int month11=11;
+int month12=12;
+
+if(month12==12||month2==2){
+System.out.println("12 is December");
+System.out.println("2 is February");
+}else{
+System.out.println("1 is January");
+System.out.println("2 is February");
+System.out.println("3 is March");
+System.out.println("4 is April");
+System.out.println("5 is May");
+System.out.println("6 is June");
+System.out.println("7 is July");
+System.out.println("8 is August");
+System.out.println("9 is September");
+System.out.println("10 is October");
+System.out.println("11 is November");
+System.out.println("12 is December");
+}
+
+
+}
+}
