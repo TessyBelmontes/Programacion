6f37c889d5c4aba8fcb16628203829e7302dc419
TessyBelmontes <tealbel@icloud.com>
2017-02-16 18:03:23 -0600

    Add files via upload
    Practica4

---
+++ Practicas/separar.java
@@ -0,0 +1,18 @@
+public class separar{
+
+public static void main(String[] args){
+
+String frase = "quiero comer pizza";
+int a = frase.indexOf(" ");
+String nfrase; 
+nfrase = frase.substring(0,a);
+
+
+System.out.println(nfrase);
+}
+
+
+
+
+
+}
---
+++ Practicas/subrutina.java
@@ -0,0 +1,17 @@
+public class subrutina{
+
+public static void printCapitalized(String a){
+String b;
+b = a.toUpperCase();
+System.out.println(b);
+
+}
+
+public static void main(String[] args){
+
+String a = "mayusculas";
+
+printCapitalized(a);
+
+}
+}
